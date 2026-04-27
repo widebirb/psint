17725a637e6a908e5f474df93c38dc3760ce59da
widebirbe <bridhone@gmail.com>
2026-04-27 14:06:23 +0800

    feat: scaffold main routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
+import { Routes, Route } from "react-router-dom"
+import LandingPage from './pages/LandingPage'
+import DashboardPage from './pages/DashboardPage'
+import AnalyticsPage from './pages/AnalyticsPage'
+
 function App() {
-  return <div>psint</div>
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/analytics" element={<AnalyticsPage />} />
+    </Routes>
+  )
 }
 
 export default App
---
+++ frontend/src/pages/AnalyticsPage.tsx
@@ -0,0 +1,7 @@
+function AnalyticsPage() {
+    return (
+        <div>AnalyticsPage</div>
+    )
+}
+
+export default AnalyticsPage
