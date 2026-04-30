64acf4c2c202b5d2fe2d34a46b3c9f794ddf1fd8
widebirbe <bridhone@gmail.com>
2026-04-30 19:41:08 +0800

    chore: set up libraries

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'sonner'
+import App from './App'
 import './index.css'
-import App from './App.tsx'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+})
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <Toaster position="bottom-right" richColors />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
